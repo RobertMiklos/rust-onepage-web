e21d0d400dbc697393b55b72f158d0fac4d62ef9
RobertMiklos <r.miklos@email.cz>
2026-01-13 23:05:03 +0100

    idk

---
+++ assets/js/about.js
@@ -0,0 +1,18 @@
+// Potřebuju dostat ty data. 
+async function aboutFetch() {
+    const response = await fetch("about.json")
+    const data = await response.json()
+} 
+
+
+// Potřebuju ty data nějak rozdělit
+
+
+
+
+// Potřebuju ty data dát do tý stránky skrze html
+
+
+
+
+// Features
